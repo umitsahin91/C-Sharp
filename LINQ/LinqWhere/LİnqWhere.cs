29b07f2fc07029a0465e0b0020fdbc517152689b
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-25 10:36:29 +0300

    Added LinqWhere project

--- LINQ/LinqWhere/LİnqWhere.cs
+++ LINQ/LinqWhere/LİnqWhere.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqWhere
 {
+    class Course
+    {
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public int Rank { get; set; }
+    }
     class LinqWhere
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Course> courses = new List<Course>()
+             {
+               new Course{
+                    ID=1,
+                    Subject="LINQ Tutorials",
+                    Rank=5
+                },
+                new Course{
+                    ID=2,
+                    Subject=".NET Threading Tutorials",
+                    Rank=4
+                },
+                new Course{
+                    ID=3,
+                    Subject="Learn WPF",
+                    Rank=3
+                }
+
+            };
+            //Query Syntax
+            var resultQuery = from course in courses
+                              where course.Rank > 3
+                              select course;
+
+            foreach (var course in resultQuery)
+            {
+                Console.WriteLine(course.Subject);
+            }
+            //Ekran Çıktısı
+            //--------------
+            //LINQ Tutorials
+            //.NET Threading Tutorials
+
+            //Method Syntax
+            var resultMethod = courses.Where(x => x.Rank > 3);
+            foreach (var course in resultMethod)
+            {
+                Console.WriteLine(course.Subject);
+            }
+            //Ekran Çıktısı
+            //--------------
+            //LINQ Tutorials
+            //.NET Threading Tutorials
+
+            //------------------------------------------------------------------//
+
+            //MULTİPLE CONDİTİONS
+
+            //Query Sytax
+            var resultMultipleQuery = from course in courses
+                                      where course.Rank > 3 && course.Subject.Contains("Threading")
+                                      select course;
+            foreach (var course in resultMultipleQuery)
+            {
+                Console.WriteLine(course.Subject);
+            }
+            //Ekran Çıktısı
+            //-------------
+            //.NET Threading Tutorials
+
+            //Method Syntax
+
+            var resultMultipleMethod = courses.Where(x => x.Rank > 3 && x.Subject.Contains("Threading"));
+            foreach (var course in resultMultipleMethod)
+            {
+                Console.WriteLine(course.Subject);
+            }
+            //Ekran Çıktısı
+            //-------------
+            //.NET Threading Tutorials
+
         }
     }
 }
